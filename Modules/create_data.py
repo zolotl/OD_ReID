19ac8f9379dcc2a2d04123b727a820483a0496ed
zolotl <jloh1000000@gmail.com>
2023-06-04 15:14:57 +0800

    changed file paths

--- Modules/create_data.py
+++ Modules/create_data.py
@@ -9,16 +9,16 @@ with open(train_map_json, 'r') as fp:
 with open(val_map_json, 'r') as fp:
     val_img_map = json.load(fp)
 
-val_img_path = r"Raw/val"
-val_ann_path = r"Raw/val_labels"
-val_cropped_img_dir = r"Processed/val_images"
+val_img_path = r"../Datasets/Raw/val"
+val_ann_path = r"../Datasets/Raw/val_labels"
+val_cropped_img_dir = r"../Datasets/Processed/val_images"
 
 crop_image(val_img_path, val_ann_path, val_cropped_img_dir, val_img_map, val_map_json)
-create_data(r'Processed/val_ann_50.txt', val_img_map)
+create_data(r'../Datasets/Processed/val_ann_50.txt', val_img_map)
 
-train_img_path = r"Raw/train"
-train_ann_path = r"Raw/train_labels"
-train_cropped_img_dir = r"Processed/train_images"
+train_img_path = r"../Datasets/Raw/train"
+train_ann_path = r"../Datasets/Raw/train_labels"
+train_cropped_img_dir = r"../Datasets/Processed/train_images"
 
 crop_image(train_img_path, train_ann_path, train_cropped_img_dir, train_img_map, train_map_json)
-create_data(r'Processed/train_ann_50.txt', train_img_map)
+create_data(r'../Datasets/Processed/train_ann_50.txt', train_img_map)
